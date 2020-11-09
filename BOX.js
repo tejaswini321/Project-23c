18171ccf150c30fd2bb53bb9394adf40fc8cc25a
tejaswini9561 <72801807+tejaswini9561@users.noreply.github.com>
2020-11-09 09:49:01 +0530

    Add files via upload

---
+++ BOX.js
@@ -0,0 +1,20 @@
+class Box{
+    constructor(){
+       var options ={
+           isStatic:true
+       }
+       this.body = Bodies.rect(200,300,200,20,options);
+       this.width = 200;
+       this.height = 20;
+
+       World.add(world,this.body);
+    }
+
+    display(){
+       var pos = this.body.position
+        rectMode(CENTER);
+        fill("red");
+        rect(pos.x,pos.y,this.width,this.height);
+
+    }
+}
